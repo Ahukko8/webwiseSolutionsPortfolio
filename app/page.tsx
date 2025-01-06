9033b1ab45e5d25b721bed392d472d4d77ce6f0d
Ahukko8 <ahukko88@gmail.com>
2025-01-06 15:13:37 +0500

    again

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,19 @@
-// page.tsx
 import { navItems } from "@/data";
-import dynamic from "next/dynamic";
 
-// Dynamically import components that might use document
-const FloatingNavDynamic = dynamic(
-  () => import("@/components/ui/FloatingNavbar").then((mod) => mod.FloatingNav),
-  { ssr: false }
-);
-
-const Hero = dynamic(() => import("@/components/Hero"), { ssr: true });
-const Grid = dynamic(() => import("@/components/Grid"), { ssr: true });
-const Footer = dynamic(() => import("@/components/Footer"), { ssr: true });
-const Clients = dynamic(() => import("@/components/Clients"), { ssr: true });
-const Approach = dynamic(() => import("@/components/Approach"), { ssr: true });
-const Experience = dynamic(() => import("@/components/Experience"), {
-  ssr: true,
-});
-const RecentProjects = dynamic(() => import("@/components/RecentProjects"), {
-  ssr: true,
-});
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Clients from "@/components/Clients";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNavDynamic navItems={navItems} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
